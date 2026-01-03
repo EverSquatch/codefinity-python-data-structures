e43a47e6b7edf0b5017fddeb0bcaf0e95fe9433f
EverSquatch <>
2026-01-03 02:31:20 +0000

    Tuple addition

---
+++ mastering_python_tuples/tuple_addition/main.py
@@ -0,0 +1,11 @@
+animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
+
+# Write your code here
+animal_movie_to_add = ("Dumbo",)
+animal_movie_to_add_2 = ("Zootopia",)
+
+animal_movies += animal_movie_to_add
+animal_movies += animal_movie_to_add_2
+
+
+print("Updated animal movies:", animal_movies)
